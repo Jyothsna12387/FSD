05b6f2ac5e6b61272eace9f54637a2562a8445fc
Jyothsna12387 <guthujyothsna@gmail.com>
2025-07-27 13:21:44 +0000

    Update roomChange.routes.js

--- src/routes/roomChange.routes.js
+++ src/routes/roomChange.routes.js
@@ -1,5 +1,10 @@
-import express from 'express';
-import { submitRoomChange, getMyRoomChangeRequests, getRoomChangeHistory } from '../controllers/roomChange.controller.js';
+ import express from 'express';
+ import { submitRoomChange,
+       getMyRoomChangeRequests, 
+       getRoomChangeHistory,
+       getAllRoomChangeRequests,
+       updateRoomChangeStatus
+     } from '../controllers/roomChange.controller.js';
 import { protect, restrictTo } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -8,7 +13,15 @@ router.post('/room-change', protect, restrictTo('student'), submitRoomChange);
 
 // Student fetches latest request
 router.get('/room-change/latest',  protect, restrictTo('student'), getMyRoomChangeRequests);
+
+// Student fetches their past requests
 router.get('/room-change/history', protect, restrictTo('student'), getRoomChangeHistory);
+
+//wardens can see the requests submitted by students
+router.get('/room-change/all', protect, restrictTo('warden'), getAllRoomChangeRequests);
+
+//warden can approve or reject the requests
+router.put('/room-change/:id/status', protect, restrictTo('warden'), updateRoomChangeStatus);
 
 
 export default router;
