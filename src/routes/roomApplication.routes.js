af68014d13babfeb1b27db86c77e8703e4578fb1
Jyothsna12387 <guthujyothsna@gmail.com>
2025-07-29 18:03:07 +0000

    Update roomApplication.routes.js

--- src/routes/roomApplication.routes.js
+++ src/routes/roomApplication.routes.js
@@ -1,15 +1,14 @@
-import express from 'express';
-import {
-  submitRoomApplication,
-//   getAllApplications,
-} from '../controllers/roomApplication.controller.js';
+  import express from 'express';
+import { submitRoomApplication,  getStudentNameByFieldId } from '../controllers/roomApplication.controller.js';
 import { protect, restrictTo } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router
-  .route('/')
-  .post(protect, restrictTo('student'), submitRoomApplication)
-//   .get(protect, restrictTo('warden', 'admin'), getAllApplications);
+// POST /api/v1/room-application
+router.post('/', protect, restrictTo('student'), submitRoomApplication);
+
+// GET /api/v1/room-application/student-name/:fieldID-->autofill student name based on fieldID
+router.get('/name/:fieldId', getStudentNameByFieldId);
+
 
 export default router;
